15242fd1759ce327f39222b37036f89393f6ba96
gompaang <hjhur004@gmail.com>
2023-05-30 13:32:25 +0900

    update seg_main.py (metric 수정)

--- seg_main.py
+++ seg_main.py
@@ -12,6 +12,8 @@ from torch.utils.data import DataLoader
 from torchvision.models import resnet50
 from torchvision.models.segmentation import fcn_resnet50
 from torchvision.datasets import VOCSegmentation
+from sklearn.metrics import confusion_matrix
+from torchvision.transforms.functional import to_tensor, to_pil_image
 
 import numpy as np
 from PIL import Image
@@ -19,16 +21,23 @@ from PIL import Image
 
 # data set
 class VOCSegDataset(VOCSegmentation):
-    def __getitem__(self, idx):
-        image, label = super().__getitem__(idx)
-        label[label > 20] = 0
-        return image, label
+    def __getitem__(self, index):
+        img = Image.open(self.images[index]).convert('RGB')
+        target = Image.open(self.masks[index])
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+            target *= 255
+            target[target > 20] = 0
+
+        return img, target
 
 
-def train(model, criterion, optimizer, num_epochs, lr):
+def train(model, criterion, optimizer, num_epochs, lr, path):
     start_time = time.time()
     best_loss = 100
     best_iou = 0
+    best_accuracy = 0.0
 
     for epoch in range(num_epochs):  # 예시로 10 에폭으로 설정
         running_loss = 0.0
@@ -40,75 +49,92 @@ def train(model, criterion, optimizer, num_epochs, lr):
 
             optimizer.zero_grad()
             outputs = model(images)['out']  # fcn_model
-            labels = labels.squeeze(1).long()
 
-            loss = criterion(outputs, labels)
+            # loss = criterion(outputs, labels)
+            loss = criterion(outputs, labels.squeeze(1).long())
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
 
-        print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss/len(train_loader):.3f}')
+        avg_loss = running_loss/len(train_loader)
+
+        if best_loss > avg_loss:
+            best_loss = avg_loss
+            torch.save(fcn_model.state_dict(), path)
+
+        print(f'[{epoch + 1}] loss: {avg_loss:.3f}')
+        print(f'Average Loss: {avg_loss:.3f}')
 
         model.eval()
-        avg_test_loss = 0.0
-        iou_list = []
+        # running_iou = 0.0
+        total_correct_pixels = 0
+        total_pixels = 0
+        total_iou = 0.0
+
         with torch.no_grad():
             for images, labels in val_loader:
                 images = images.to(device)
                 labels = labels.to(device)
 
-                outputs = model(images)['out']  # fcn_model
-                labels = labels.squeeze(1).long()
+                # outputs = model(images)['out']  # fcn_model
+                # predicted_masks = torch.argmax(outputs, dim=1)
 
-                loss = criterion(outputs, labels)
-                avg_test_loss += loss.item()
+                # intersection = torch.logical_and(predicted_masks, labels.squeeze(1))
+                # union = torch.logical_or(predicted_masks, labels.squeeze(1))
+                # iou = torch.sum(intersection.float()) / torch.sum(union.float())
 
-                iou = calculate_iou(outputs, labels, num_classes)
-                iou_list.append(iou)
+                pred_mask = model(images)['out']
+                pred_mask = torch.argmax(pred_mask, dim=1)
+                pred_mask = pred_mask.detach().cpu().numpy()
+                target_mask = labels.detach().cpu().numpy()
 
+                iou = compute_iou(pred_mask, target_mask)
+                total_iou += iou
 
-        miou = np.mean(iou_list)
+        avg_iou = total_iou / len(val_loader)
+        # avg_accuracy = total_correct_pixels / total_pixels
 
-        if best_loss > (avg_test_loss / len(val_loader)):
-            best_loss = (avg_test_loss / len(val_loader))
+        if best_iou < avg_iou:
+            best_iou = avg_iou
+        # if best_accuracy < avg_accuracy:
+        #     best_accuracy = avg_accuracy
 
-        if best_iou < miou:
-            best_iou = miou
-
-        print(f'Average Loss: {avg_test_loss / len(val_loader):.3f}')
-        print(f'Average iou: {miou:.3f}')
-        wandb.log({'MIoU': miou, 'loss': avg_test_loss / len(val_loader)})
+        print(f'avg iou: {iou:.3f}')
+        # print(f'avg pixel accuracy: {avg_accuracy:.3f}')
+        wandb.log({'iou': iou, 'loss': avg_loss})
+        # wandb.log({'pixel accuracy': avg_accuracy, 'loss': avg_loss})
 
     total_time = time.time() - start_time
     print('Finish Training')
     print('Training complete in {:.0f}m {:.0f}s'.format(total_time // 60, total_time % 60))
-    print('Best val loss: {:4f}'.format(best_loss))
-    print('Best miou: {:4f}'.format(best_iou))
+    print('Best loss: {:4f}'.format(best_loss))
+    # print('Best miou: {:4f}'.format(best_miou))
+    print('Best accuracy: {:4f}'.format(best_accuracy))
 
 
-def calculate_iou(outputs, targets, num_classes):
-    pred = outputs.argmax(dim=1)
-    iou_list = []
-    for cls in range(num_classes):
-        pred_cls = pred == cls
-        target_cls = targets == cls
-        intersection = (pred_cls & target_cls).sum().item()
-        union = (pred_cls | target_cls).sum().item()
-        iou = intersection / (union + 1e-10)  # avoid division by zero
-        iou_list.append(iou)
-    mean_iou = np.mean(iou_list)
-    return mean_iou
+def compute_iou(pred_mask, target_mask):
+    pred_mask = torch.from_numpy(pred_mask)
+    target_mask = torch.from_numpy(target_mask)
+    intersection = torch.logical_and(pred_mask, target_mask).sum().item()
+    union = torch.logical_or(pred_mask, target_mask).sum().item()
+    iou = intersection / union
+    return iou
 
 
-def pixel_accuracy(pred, target):
-    # 모델의 예측 클래스 가져오기
-    _, pred_labels = torch.max(pred, dim=1)
+def compute_iou2(outputs, targets):
+    predicted_masks = torch.argmax(outputs, dim=1)
+    predicted_masks = F.one_hot(predicted_masks, num_classes=21).permute(0, 3, 1, 2).float()
+    intersection = torch.sum(predicted_masks * targets)
+    union = torch.sum(predicted_masks) + torch.sum(targets) - intersection
+    iou = intersection / union
+    return iou
 
-    # 타깃과 예측 결과의 픽셀 단위 정확도 계산
-    correct_pixels = torch.sum(pred_labels == target).item()
-    total_pixels = target.numel()
+
+def pixel_accuracy(outputs, targets):
+    _, predicted = torch.max(outputs, 1)
+    correct_pixels = (predicted == targets).sum().item()
+    total_pixels = targets.numel()
     accuracy = correct_pixels / total_pixels
-
     return accuracy
 
 
@@ -151,10 +177,10 @@ if __name__ == '__main__':
     ])
 
     train_set = VOCSegDataset(
-        root='./data', year='2012', image_set='train', download=True,
+        root='./data', year='2012', image_set='train', download=False,
         transform=image_transforms, target_transform=target_transforms)
     val_set = VOCSegDataset(
-        root='./data', year='2012', image_set='val', download=True,
+        root='./data', year='2012', image_set='val', download=False,
         transform=image_transforms, target_transform=target_transforms)
 
     # np.random.seed(0)
@@ -180,25 +206,28 @@ if __name__ == '__main__':
         fcn_model.backbone.load_state_dict(state_dict)
 
         fcn_model = fcn_model.to(device)
+        # criterion = DiceLoss()
         criterion = nn.CrossEntropyLoss()
         # criterion = nn.BCEWithLogitsLoss()
+        # optimizer = torch.optim.SGD(fcn_model.parameters(), lr=0.001, momentum=0.9)
         optimizer = optim.Adam(fcn_model.parameters(), lr=lr)
 
         # 3. train
-        train(fcn_model, criterion, optimizer, num_epochs, lr)
-        torch.save(fcn_model.state_dict(), path)
+        train(fcn_model, criterion, optimizer, num_epochs, lr, path)
+        # torch.save(fcn_model.state_dict(), path)
 
     elif ptmodel == 'basic':
         # 2. model
-        fcn_model = fcn_resnet50(pretrained=False, num_classes=num_classes)
+        fcn_model = fcn_resnet50(pretrained=True, num_classes=num_classes)
         fcn_model.to(device)
+        # criterion = DiceLoss()
         criterion = nn.CrossEntropyLoss()
         # criterion = nn.BCEWithLogitsLoss()
         optimizer = optim.Adam(fcn_model.parameters(), lr=lr)
 
         # 3. train
-        train(fcn_model, criterion, optimizer, num_epochs, lr)
-        torch.save(fcn_model.state_dict(), path)
+        train(fcn_model, criterion, optimizer, num_epochs, lr, path)
+        # torch.save(fcn_model.state_dict(), path)
 
 
 
